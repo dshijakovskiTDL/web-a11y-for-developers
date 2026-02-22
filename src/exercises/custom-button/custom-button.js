fbfad674ae18fd3cbc4fed7677774138717043f8
Daniel Shijakovski <daniel.shijakovski@testdevlab.com>
2026-02-22 22:45:02 +0100

    Day 1: Add solutions

--- src/exercises/custom-button/custom-button.js
+++ src/exercises/custom-button/custom-button.js
@@ -1,1 +1,16 @@
-// TODO: Add the proper click event handlers to the custom button
+// Add the proper click event handlers to the custom button
+
+const customButtons = document.querySelectorAll('div[role="button"]');
+
+const onClick = () => alert('The custom button is working!');
+
+customButtons.forEach((button) => {
+  button.addEventListener('click', onClick);
+
+  button.addEventListener('keyup', (e) => {
+    if (e.key === ' ' || e.key === 'Enter') {
+      e.preventDefault();
+      onClick();
+    }
+  });
+});
