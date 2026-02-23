6b8008377fced08764735122bb9de3bbd30d82ee
Daniel Shijakovski <daniel.shijakovski@testdevlab.com>
2026-02-23 23:04:44 +0100

    Better Forms instructions

---
+++ src/exercises/form-live-regions/form-live.regions.js
@@ -0,0 +1,5 @@
+// TODO: Add the appropriate form validation such that the form does not allow:
+// TODO: - Empty "email" or "password" fields
+// TODO: - Emails that don't conform to the following RegEx pattern: https://uibakery.io/regex-library/email#:~:text=be%20IP%20address.-,/%5E%5Ba%2Dz0%2D9!%23%24%25%26%27*%2B/%3D%3F%5E_%60%7B%7C%7D~%2D%5D%2B(%3F%3A%5C.%5Ba%2Dz0%2D9!%23%24%25%26%27*%2B/%3D%3F%5E_%60%7B%7C%7D~%2D%5D%2B)*%40(%3F%3A%5Ba%2Dz0%2D9%5D(%3F%3A%5Ba%2Dz0%2D9%2D%5D*%5Ba%2Dz0%2D9%5D)%3F%5C.)%2B%5Ba%2Dz0%2D9%5D(%3F%3A%5Ba%2Dz0%2D9%2D%5D*%5Ba%2Dz0%2D9%5D)%3F%24/,-Test%20it!
+// TODO: - Emails that are not of the "company-domain.com" domain
+// TODO: - Passwords that contain less than 12 characters
