a19fba67da6c056967f4ae86c3d2234f36b38954
Daniel Shijakovski <daniel.shijakovski@testdevlab.com>
2026-02-23 21:01:53 +0100

    Accordion solution

--- src/exercises/accordions/accordions.js
+++ src/exercises/accordions/accordions.js
@@ -1,1 +1,23 @@
-// TODO: Add the missing functionality needed to have only 1 Accordion open at a time
+// Add the missing functionality needed to have only 1 Accordion open at a time
+const accordionGroups = document.querySelectorAll('.accordion-group');
+
+accordionGroups.forEach((accordionGroup) => {
+  const triggerButtons = accordionGroup.querySelectorAll('.trigger');
+
+  triggerButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      triggerButtons.forEach((trigger) => {
+        if (e.target === trigger) {
+          toggleAccordion(trigger);
+        } else {
+          trigger.setAttribute('aria-expanded', 'false');
+        }
+      });
+    });
+  });
+});
+
+function toggleAccordion(trigger) {
+  const isOpen = trigger.getAttribute('aria-expanded') === 'true';
+  trigger.setAttribute('aria-expanded', String(!isOpen));
+}
